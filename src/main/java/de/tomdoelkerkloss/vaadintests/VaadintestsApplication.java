0d78e4458711b2f3c86a9522abcab25a8389e5d0
Tom K <tomf258@me.com>
2019-07-09 22:40:19 +0200

    Add files via upload

---
+++ src/main/java/de/tomdoelkerkloss/vaadintests/VaadintestsApplication.java
@@ -0,0 +1,13 @@
+package de.tomdoelkerkloss.vaadintests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadintestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadintestsApplication.class, args);
+	}
+
+}
